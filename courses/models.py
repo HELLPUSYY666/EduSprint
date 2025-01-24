fddf98ee152628b62bb7242998e5ab793487c0ee
Zakariya <pzakariya@mail.ru>
2025-01-24 22:59:34 +0500

    Добавил модель courses models.py Lesson

--- courses/models.py
+++ courses/models.py
@@ -10,3 +10,9 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
+    title = models.CharField(max_length=255)
+    video_url = models.URLField()
+    content = models.TextField()
+
+
