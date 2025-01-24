2927c03aa912625b72904751f9e0871d3be24938
Zakariya <pzakariya@mail.ru>
2025-01-24 23:08:32 +0500

    Добавил модель progress models.py Progress

--- progress/models.py
+++ progress/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import CustomUser
+from courses.models import Lesson
 
-# Create your models here.
+
+class Progress(models.Model):
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='progress')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='progress')
+    completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(auto_now_add=True, null=True)
+
