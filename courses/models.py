19c2f10c8c13fdfa54c7546bcb2fbe9388723794
Zakariya <pzakariya@mail.ru>
2025-01-24 22:58:09 +0500

    Добавил модель courses models.py Course

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    instructor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='instructed_courses')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Lesson(models.Model):
