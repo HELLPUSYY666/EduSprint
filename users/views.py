48511d528d29185b6c3cefec213a6cf9a84d01f6
Zakariya <pzakariya@mail.ru>
2025-02-02 21:59:31 +0500

    создал представление в юсерс под отпарвку рассылки

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .service import send_welcome_email
 
-# Create your views here.
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = '/'
+    template_name = 'users/usercreate.html'
+
+    def form_valid(self, form):
+        form.save()
+        send_welcome_email(form.instance.email)
+        return super().form_valid(form)
