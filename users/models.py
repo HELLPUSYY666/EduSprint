bdaaca73dce4ea2b74ee9616b085247ca5cc60ab
Zakariya <pzakariya@mail.ru>
2025-02-02 21:15:58 +0500

    Добавил эмаил и имя для пользователя. Для рассылки на эмейл. Так же создал для этого дела админку

--- users/models.py
+++ users/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    name = models.CharField(max_length=120)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
