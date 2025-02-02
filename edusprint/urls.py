a1c305b2e9ebec1bec3ab5de0a6ec060072bb69a
Zakariya <pzakariya@mail.ru>
2025-02-02 21:58:40 +0500

    Добавил в урлы проекта все приложения

--- edusprint/urls.py
+++ edusprint/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('certificates/v1/', include('certificates.urls')),
-
+    path('courses/v1/', include('courses.urls')),
+    path('progress/v1/', include('progress.urls')),
+    path('users/v1/', include('users.urls')),
 ]
