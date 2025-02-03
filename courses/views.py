6253a7915c9b308da3b7bbc4b22d59bd43e2eb49
Zakariya <pzakariya@mail.ru>
2025-02-03 15:22:20 +0500

    Добавил представления по CBV модели в courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,26 @@
+from rest_framework.generics import ListAPIView
 from django.shortcuts import render
 
-# Create your views here.
+from courses.models import Course, Lesson, Quiz
+from courses.serializer import CourseSerializer, LessonSerializer, QuizSerializer
+
+
+class CourseListView(ListAPIView):
+    serializer_class = CourseSerializer
+
+    def get_queryset(self):
+        return Course.objects.all()
+
+
+class LessonListView(ListAPIView):
+    serializer_class = LessonSerializer
+
+    def get_queryset(self):
+        return Lesson.objects.all()
+
+
+class QuizListView(ListAPIView):
+    serializer_class = QuizSerializer
+
+    def get_queryset(self):
+        return Quiz.objects.all()
