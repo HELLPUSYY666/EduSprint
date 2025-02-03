6d6943f75605d706f69fce44bfd2c1b50c1663c4
Zakariya <pzakariya@mail.ru>
2025-02-03 16:04:44 +0500

    Добавил ckeditor в users в админку

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
 
-# Проверяем, зарегистрирован ли User в админке
+
+class UserAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget(), required=False)
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class CustomUserAdmin(UserAdmin):
+    form = UserAdminForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('content',)}),
+    )
+
+
 if not admin.site.is_registered(User):
-    admin.site.register(User, UserAdmin)
+    admin.site.register(User, CustomUserAdmin)
