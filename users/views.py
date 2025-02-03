585c43b743572f6254395135701a112c5d9a9845
Zakariya <pzakariya@mail.ru>
2025-02-03 15:22:33 +0500

    Добавил представления по CBV модели в users

--- users/views.py
+++ users/views.py
@@ -2,7 +2,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import CreateView
+
+from .serializer import UserSerializer
 from .service import send_welcome_email
+from .tasks import send_welcome_email
+from rest_framework.generics import ListAPIView
 
 
 class UserCreateView(CreateView):
@@ -13,5 +17,13 @@ class UserCreateView(CreateView):
 
     def form_valid(self, form):
         form.save()
-        send_welcome_email(form.instance.email)
+        # send_welcome_email(form.instance.email)
+        send_welcome_email.delay(form.instance.email)
         return super().form_valid(form)
+
+
+class UserListView(ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all()
