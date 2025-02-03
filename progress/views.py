4d7ffc15c3073d97e8ca8385239e81f1b873120e
Zakariya <pzakariya@mail.ru>
2025-02-03 15:22:25 +0500

    Добавил представления по CBV модели в progress

--- progress/views.py
+++ progress/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .serializer import *
 
-# Create your views here.
+
+class ProgressView(ListAPIView):
+    serializer_class = ProgressSerializer
+
+    def get_queryset(self):
+        return Progress.objects.all()
