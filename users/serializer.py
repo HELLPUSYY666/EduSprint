51aff2aff7d6bc2208eee875ba6d0c25268ba706
Zakariya <pzakariya@mail.ru>
2025-02-03 15:20:20 +0500

    Добавил сериалайзеры в users

---
+++ users/serializer.py
@@ -0,0 +1,10 @@
+from django.core.validators import MaxLengthValidator
+from rest_framework import serializers
+from .models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
