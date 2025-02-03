439f2c37d9fd108da7b57aa04897d3c01dab2a5e
Zakariya <pzakariya@mail.ru>
2025-02-03 17:17:34 +0500

    Добавил урлы для пользователей и курсов

--- courses/urls.py
+++ courses/urls.py
@@ -2,5 +2,18 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', CourseListView.as_view(), name='course-list'),
+    path('courses/', CourseListView.as_view(), name='course-list'),
+    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
+    path('courses/create/', CourseCreateView.as_view(), name='course-create'),
+    path('courses/<int:pk>/update/', CourseUpdateView.as_view(), name='course-update'),
+
+    path('lessons/', LessonListView.as_view(), name='lesson-list'),
+    path('lessons/<int:pk>/', LessonDetailView.as_view(), name='lesson-detail'),
+    path('lessons/create/', LessonCreateView.as_view(), name='lesson-create'),
+    path('lessons/<int:pk>/update/', LessonUpdateView.as_view(), name='lesson-update'),
+
+    path('quizzes/', QuizListView.as_view(), name='quiz-list'),
+    path('quizzes/<int:pk>/', QuizDetailView.as_view(), name='quiz-detail'),
+    path('quizzes/create/', QuizCreateView.as_view(), name='quiz-create'),
+    path('quizzes/<int:pk>/update/', QuizUpdateView.as_view(), name='quiz-update'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from .views import *
 urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),
     path('user/create/', UserCreateView.as_view(), name='user-create'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
