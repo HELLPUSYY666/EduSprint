36995fb0e32fb52818ed3f242302bdf65b5536b6
Zakariya <pzakariya@mail.ru>
2025-02-03 15:20:10 +0500

    Добавил сериалайзеры в course

---
+++ courses/serializer.py
@@ -0,0 +1,22 @@
+from django.core.validators import MaxLengthValidator
+from rest_framework import serializers
+from .models import *
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = '__all__'
+
