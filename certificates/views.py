cdc1bbfc54725554a5d5b2c7e7568eeec1a58b97
Zakariya <pzakariya@mail.ru>
2025-02-03 15:22:15 +0500

    Добавил представления по CBV модели в certificates

--- certificates/views.py
+++ certificates/views.py
@@ -1,3 +1,11 @@
+from rest_framework.generics import ListAPIView
 from django.shortcuts import render
+from certificates.models import Certificate
+from .serializer import CertificateSerializer
 
-# Create your views here.
+
+class CertificateListView(ListAPIView):
+    serializer_class = CertificateSerializer
+
+    def get_queryset(self):
+        return Certificate.objects.all()
