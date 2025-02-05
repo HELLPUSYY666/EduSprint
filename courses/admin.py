01ab5f3159bb8be439b3f4cf9d9300f25a1fa9c6
Zakariya <pzakariya@mail.ru>
2025-02-05 16:45:07 +0500

    Создал админку для курсов

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Course
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
+
+    class Meta:
+        model = Course
+        fields = '__all__'
