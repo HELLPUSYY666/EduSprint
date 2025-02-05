c743e67749e000cdd4de0c8de043893b2880388f
Zakariya <pzakariya@mail.ru>
2025-02-05 18:09:21 +0500

    Написал первый тест для проверки создания курсов

--- courses/tests/test_views.py
+++ courses/tests/test_views.py
@@ -3,6 +3,9 @@ import django
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from courses.views import *
+from datetime import datetime
+from courses.models import Course
+from users.models import CustomUser
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edusprint.settings")
 django.setup()
@@ -10,7 +13,16 @@ django.setup()
 
 class CourseListViewTest(APITestCase):
     def test_course_list(self):
+        instructor = CustomUser.objects.create_user(username="ZAKA", password="password123")
+        course1 = Course.objects.create(title='Django - 1', description='This is the django course',
+                                        instructor=instructor,
+                                        created_at=datetime.now())
+        course2 = Course.objects.create(title='Django - 2', description='This is the second django course',
+                                        instructor=instructor,
+                                        created_at=datetime.now())
         url = reverse('course-list')
-        print(url)
         response = self.client.get(url)
-        print(response)
+        print(url)
+        print(response.status_code)
+        self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
+        self.assertEqual(response.status_code, 200)
